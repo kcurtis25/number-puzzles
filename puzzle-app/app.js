760c13cc23d12444dc7fee50f490371fd9ef872b
Phil Howlin <phowlin@localhost.localdomain>
2016-02-02 07:13:45 +1100

    Update allows web client requests to /pdf to display ./output/test.pdf

--- puzzle-app/app.js
+++ puzzle-app/app.js
@@ -9,6 +9,7 @@ var routes        = require('./routes/index');
 var users         = require('./routes/users');
 var about         = require('./routes/about');
 var generate      = require('./routes/generate');
+var pdf           = require('./routes/pdf');
 
 var app           = express();
 
@@ -28,6 +29,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/about', about);
 app.use('/generate', generate);
+app.use('/pdf', pdf);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
