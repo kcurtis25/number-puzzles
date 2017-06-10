dcc50c59c0ee69bbc4c03ac406219615efdaa9c8
Katherine Curtis <katherine.curtis@hiivesystems.com>
2017-06-10 16:46:24 +1000

    Allow specifying min and max when generating datafiles

--- bin/generate_datafile.pl
+++ bin/generate_datafile.pl
@@ -3,11 +3,28 @@
 use strict;
 use warnings;
 
+use Getopt::Long;
+
 use FindBin qw/$Bin/;
 use lib "$Bin/../lib";
 
 use SCP::ProblemGenerator;
 
+my $usage = "Usage: generate_datafile.pl [ -t type ] [ --min min-result ] [ --max max-result ]";
+
+my ($type, $min_result, $max_result);
+die $usage unless GetOptions(
+    'type=s'       => \$type,
+    'min-result=s' => \$min_result,
+    'max-result=s' => \$max_result,
+);
+
+$type ||= 'Addition';
+
+my $args = {};
+$args->{min_result} = $min_result if $min_result;
+$args->{max_result} = $max_result if $max_result;
+
 # Set as a global, with default values
-my $generator = SCP::ProblemGenerator->new();
+my $generator = SCP::ProblemGenerator->new($args);
 $generator->print();
